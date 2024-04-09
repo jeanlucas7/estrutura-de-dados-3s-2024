7efc8f77b758c18d90cbb2d909faed334285d42c
jeanlucas7 <acesso.jeanlucas@gmail.com>
2024-04-09 11:23:08 -0300

    atividade 3

---
+++ atividade3/bubble_sort.c
@@ -0,0 +1,16 @@
+void swap (int* n1, int* n2){
+    int aux = *n1;
+    *n1 = *n2;
+    *n2 = aux;
+}
+
+void bubble_sort (int arr[], int n){
+    for(int i = 0; i < n; i++){
+        arr[i]; 
+        for (int j = i; j < n; j++){
+            if (arr[i] > arr[j]){
+                swap(&arr[i], &arr[j]);
+            }
+        }
+    }
+}
